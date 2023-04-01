92b4608d8563a0e81fbe9fe135115205d5a8507d
Khoi Tran <tranvankhoi2002@gmail.com>
2023-03-31 21:20:38 -0700

    axios call

--- src/pages/CreateEditPage/CreateEditPage.jsx
+++ src/pages/CreateEditPage/CreateEditPage.jsx
@@ -1,24 +1,40 @@
-import {React, useState} from 'react'
+import { React, useState } from "react";
+import axios from "axios"
+import { element } from "prop-types";
+const list = []
+let counter = 0
 
 function CreateEditPage() {
   const [value, setValue] = useState("");
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     setValue(e.target.value);
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    alert("you have searched for - " + value);
-    // or you can send data to backend
+    axios
+    .get(`http://localhost:8080/singles/${value}`)
+    .then((res) => {
+      console.log(res)
+      list.push({
+        id:res.data.id,
+        name:res.data.name,
+        img:res.data.img
+    })
+    console.log(list.name)
+    })
+
+    setValue("");
   };
 
-  const handleKeypress = e => {
-      //it triggers by pressing the enter key
+  const handleKeypress = (e) => {
+    // 13 keycode is enter key
     if (e.keyCode === 13) {
       handleSubmit();
     }
   };
+
 
   return (
     <>
@@ -33,12 +49,19 @@ function CreateEditPage() {
         <button onClick={handleSubmit} type="submit">
           Search
         </button>
-      </form>
-
-      
+      </form> 
+      {list.map(element=>{
+        {console.log(element)}
+        return(
+          <div key={element.id}>
+            <p>{element.id}</p>
+            <p>{element.name}</p>
+            <p>{element.img}</p>
+          </div>
+        )
+      })}     
     </>
   );
 }
 
-export default CreateEditPage
-
+export default CreateEditPage;
